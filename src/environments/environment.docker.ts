48f98a4cc646975f33a4983409751fe148b47184
liuss <liuss@visionet.com.cn>
2018-01-18 16:29:57 +0800

    新增docker构建时的配置文件

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,13 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  apiPath: '/push-logic',
+  production: false,
+  pushServer: '',
+  pushScirpt: '/push',
+  pushOptionPath: '/push/socket.io/',
+  copyRight: '上海xxx信息技术股份有限公司'
+};
