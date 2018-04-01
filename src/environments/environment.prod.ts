55fbdbe6c60bcd7c7832d57dceb233633d65c1db
samzong.lu <samzong.lu@gmail.com>
2018-04-01 15:43:35 +0800

    修改package.json 与environment.prod.ts 域名配置信息，修改为对应ipush.onesykes.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  apiPath: '//ipush.weisk.com/push-logic',
+  apiPath: '//ipush.onesykes.com/push-logic',
   production: true,
-  pushServer: '//ipush.weisk.com',
-  pushScirpt: '//ipush.weisk.com/push',
+  pushServer: '//ipush.onesykes.com',
+  pushScirpt: '//ipush.onesykes.com/push',
   pushOptionPath: '/push/socket.io/',
   copyRight: '上海yyy信息技术股份有限公司'
 };
