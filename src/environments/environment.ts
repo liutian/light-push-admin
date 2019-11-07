2f2d57ad44c18315b5766ff006788b557b74f3c0
付强 <qiang.fu@baozun.com>
2019-11-07 15:19:29 +0800

    开发环境配置

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,13 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+
+// 推送服务器地址： http://10.11.112.14:8080/push-logic
 export const environment = {
-  apiPath: 'http://127.0.0.1:10002',
+  apiPath: 'http://192.168.106.128:10002',
   production: false,
-  pushServer: 'http://127.0.0.1:21314',
-  pushScirpt: 'http://127.0.0.1:21314',
+  pushServer: 'http://192.168.106.128:21314',
+  pushScirpt: 'http://192.168.106.128:21314',
   pushOptionPath: '/socket.io/',
   copyRight: '上海xxx信息技术股份有限公司'
 };
